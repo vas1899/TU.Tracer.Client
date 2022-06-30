4e2c5f8e8d363e6760e61c0f57cdf517b0f5100a
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-30 03:05:51 +0300

    Add accoun to Nav and Home

--- client-app/src/app/layout/features/homePage/HomePage.tsx
+++ client-app/src/app/layout/features/homePage/HomePage.tsx
@@ -1,22 +1,28 @@
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { Segment, Container, Header, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../../stores/store";
 
 export default observer(function HomePage() {
+  const { accountStore } = useStore();
   return (
     <>
       <Segment inverted textAlign="center" vertical className="masthead">
         <Container text>
           <Header as="h1" inverted>
             <Image size="massive" src="/assets/logo.png" alt="logo" style={{ marginBottom: 12 }} />
-            Repackets
+            Packet Tracer
           </Header>
-
-          <Header as="h2" inverted content="Welcome to Repackets" />
-
-          <Button as={Link} to="/login" size="huge" inverted>
-            Login
-          </Button>
+          <Header as="h2" inverted content="Welcome" />
+          {accountStore.isLoggedIn ? (
+            <Button as={Link} to="/packets" size="huge" inverted>
+              Go to Packets
+            </Button>
+          ) : (
+            <Button as={Link} to="/login" size="huge" inverted>
+              Login
+            </Button>
+          )}
         </Container>
       </Segment>
     </>
--- client-app/src/app/layout/features/navbar/NavBar.tsx
+++ client-app/src/app/layout/features/navbar/NavBar.tsx
@@ -1,19 +1,34 @@
 import { observer } from "mobx-react-lite";
 import { Link, NavLink } from "react-router-dom";
-import { Button, Icon, Menu } from "semantic-ui-react";
+import { Button, Icon, Menu, Image, Dropdown, Container } from "semantic-ui-react";
+import { useStore } from "../../../stores/store";
 
 export default observer(function NavBar() {
+  const {
+    accountStore: { user, logout },
+  } = useStore();
   return (
     <Menu inverted fixed="top">
-      <Menu.Item as={NavLink} to="/" exact>
-        <Icon name="users" circular />
-        Socialfy
-      </Menu.Item>
-      <Menu.Item as={NavLink} to="/packets" name="Packets" />
-      <Menu.Item>
-        <Button as={NavLink} to="/createPacket" positive content="Create" />
-      </Menu.Item>
-      <Menu.Item as={Link} to="/errors" name="Testing" />
+      <Container>
+        <Menu.Item as={NavLink} to="/" exact>
+          <Icon name="users" circular />
+          Socialfy
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/packets" name="Packets" />
+        <Menu.Item>
+          <Button as={NavLink} to="/createPacket" positive content="Create" />
+        </Menu.Item>
+        <Menu.Item as={Link} to="/errors" name="Testing" />
+        <Menu.Item position="right">
+          <Image src={user?.image || "/assets/user.png"} avatar spaced="right" />
+          <Dropdown pointing="top left" text={user?.displayName}>
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to={`/accounts/${user?.username}`} text="Account" />
+              <Dropdown.Item onClick={logout} icon="power" />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
+      </Container>
     </Menu>
   );
 });
